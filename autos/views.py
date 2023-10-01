0e301b2190d830730683b8ffa69361eef8bfec88
Jon Riley <jonathanriley@Jonathans-MacBook-Air.local>
2023-09-30 21:33:33 -0500

    refactor autos code

--- autos/views.py
+++ autos/views.py
@@ -3,50 +3,39 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
 from autos.models import Auto, Make
 from autos.forms import MakeForm
 
+import random
+
 # Create your views here.
-
-
 class MainView(LoginRequiredMixin, View):
     def get(self, request):
         mc = Make.objects.count()
         ml = Make.objects.all()
         al = Auto.objects.all()
-
         ctx = {'make_count': ml.count(), 'auto_list': al, 'make_list': ml}
         return render(request, 'autos/auto_list.html', ctx)
-
 
 class MakeView(LoginRequiredMixin, View):
     def get(self, request):
         ml = Make.objects.all()
-        ctx = {'make_list': ml}
+        ctx = {'make_list': ml, 'make_count': ml.count()}
         return render(request, 'autos/make_list.html', ctx)
-
 
 # We use reverse_lazy() because we are in "constructor attribute" code
 # that is run before urls.py is completely loaded
-class MakeCreate(LoginRequiredMixin, View):
-    template = 'autos/make_form.html'
+
+class MakeCreate(LoginRequiredMixin, CreateView):
+    model = Make
+    fields = '__all__'
     success_url = reverse_lazy('autos:all')
+    template_name = 'autos/make_form_b.html'
 
-    def get(self, request):
-        form = MakeForm()
-        ctx = {'form': form}
-        return render(request, self.template, ctx)
-
-    def post(self, request):
-        form = MakeForm(request.POST)
-        if not form.is_valid():
-            ctx = {'form': form}
-            return render(request, self.template, ctx)
-
-        make = form.save()
-        return redirect(self.success_url)
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["number"] = random.randrange(1, 100)
+        return context
 
 # MakeUpdate has code to implement the get/post/validate/store flow
 # AutoUpdate (below) is doing the same thing with no code
